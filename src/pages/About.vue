75ca33a27cc3aab45efc77e61c6de59661e7532f
Scott Carr <prescottcarr@gmail.com>
2020-05-27 23:23:26 -0400

    Test
    j

---
+++ src/pages/About.vue
@@ -0,0 +1,35 @@
+<template>
+  <Layout :show-logo="false">
+
+    <div v-for="edge in $page.posts.edges" :key="edge.node.id">
+      <h2>{{ edge.node.title }}</h2>
+    </div>
+
+
+  </Layout>
+</template>
+
+<page-query>
+query {
+  posts: allPost {
+    edges {
+      node {
+        id
+        title
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+
+export default {
+  components: {
+
+  },
+  metaInfo: {
+    title: 'About'
+  }
+}
+</script>
