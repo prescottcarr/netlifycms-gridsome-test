6ca1b326ef243038b0b5c076711d67f0bef3a6e1
Scott Carr <prescottcarr@gmail.com>
2020-05-28 20:00:05 -0400

    s

--- gridsome.config.js
+++ gridsome.config.js
@@ -29,7 +29,22 @@ module.exports = {
         }
       }
     },
+    {
+      use: 'gridsome-plugin-tailwindcss',
+      /**
+      * These are the default options. You don't need to set any options to get
+      * going. Seriously, you don't need to declare tailwind.config.js.
 
+      options: {
+        tailwindConfig: './tailwind.config.js',
+        purgeConfig: {},
+        presetEnvConfig: {},
+        shouldPurge: true,
+        shouldImport: true,
+        shouldTimeTravel: true
+      }
+      */
+    },
     {
       // Create posts from markdown files
       use: '@gridsome/source-filesystem',
