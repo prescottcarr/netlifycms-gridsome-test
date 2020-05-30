f4eb2b19b73a17bea6df8d6cd04f99a483cce062
Scott Carr <prescottcarr@gmail.com>
2020-05-29 21:46:39 -0400

    c

---
+++ src/pages/Contact.vue
@@ -0,0 +1,37 @@
+<template>
+  <Layout :show-logo="false">
+
+    <h1>{{ $page.section.title }}</h1>
+    <p>{{ $page.section.short}}</p>
+
+    <div v-for="person in $page.section.locations">
+      <h3>{{ person.name }}</h3>
+      <p>{{ person.address }}</p>
+    </div>
+
+  </Layout>
+</template>
+
+
+<page-query>
+  query {
+    section (path:"/content/section/locations/") {
+      title
+      intro
+      locations {
+        name
+        address
+      }
+    }
+  }
+</page-query>
+
+<script>
+
+export default {
+
+  metaInfo: {
+    title: 'About'
+  }
+}
+</script>
