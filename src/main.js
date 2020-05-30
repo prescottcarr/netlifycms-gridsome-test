1c584463799e56139c7c8c161a5681165b0665f3
Scott Carr <prescottcarr@gmail.com>
2020-05-30 00:11:16 -0400

    f

--- src/main.js
+++ src/main.js
@@ -2,10 +2,43 @@
 
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from '~/layouts/Default.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function (Vue, { router, head, isClient }) {
 
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyCGRG6k8vex6Zru7xujl2LdOjjytDVpFq8',
+      libraries: 'places', // This is required if you use the Autocomplete plugin
+      // OR: libraries: 'places,drawing'
+      // OR: libraries: 'places,drawing,visualization'
+      // (as you require)
+
+      //// If you want to set the version, you can do so:
+      // v: '3.26',
+    },
+
+    //// If you intend to programmatically custom event listener code
+    //// (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
+    //// instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
+    //// you might need to turn this on.
+    // autobindAllEvents: false,
+
+    //// If you want to manually install components, e.g.
+    //// import {GmapMarker} from 'vue2-google-maps/src/components/marker'
+    //// Vue.component('GmapMarker', GmapMarker)
+    //// then set installComponents to 'false'.
+    //// If you want to automatically install all the components this property must be set to 'true':
+    installComponents: true
+  })
+
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700&display=swap'
+  })
+  
 }
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -4,9 +4,7 @@
     <h1>{{ $page.section.title }}</h1>
     <p>{{ $page.section.short}}</p>
 
-
     <div>{{ $page.section.hours}}</div>
-    <p>{{ $page.section.map.coordinates}}</p>
     <p>{{ $page.section.phone}}</p>
     <p>{{ $page.section.email}}</p>
     <p>{{ $page.section.address1}}</p>
@@ -16,6 +14,19 @@
     <p>{{ $page.section.country}}</p>
     <p>{{ $page.section.postal}}</p>
 
+    <GmapMap
+    :center="{lat:19.2985383, lng:-81.3676755}"
+    :zoom="13"
+    map-type-id="roadmap"
+    style="width: 400px; height: 250px"
+    >
+    <GmapMarker
+    :position="{lat:19.2985383, lng:-81.3676755}"
+    :clickable="true"
+    @click="websiteUrl"
+    />
+  </GmapMap>
+
 
   </Layout>
 </template>
@@ -23,13 +34,10 @@
 
 <page-query>
   query {
-    section (path:"/content/section/locations/") {
+    section (path:"/content/section/contact/") {
       title
       short
       hours
-      map {
-        coordinates
-      }
       phone
       email
       address1
@@ -47,7 +55,12 @@
 export default {
 
   metaInfo: {
-    title: 'About'
+    title: 'Contact Us'
+  },
+  methods: {
+    websiteUrl: function(event) {
+        window.open("https://www.google.com/maps/dir//Hew's+Cleaning+Services,+335+Dorcy+Dr,+George+Town,+Cayman+Islands/@19.2985383,-81.3676755,15z/data=!4m16!1m6!3m5!1s0x0:0x1da08c8686cc4f6e!2sHew's+Cleaning+Services!8m2!3d19.2985383!4d-81.3676755!4m8!1m0!1m5!1m1!1s0x8f258643fee1623d:0x1da08c8686cc4f6e!2m2!1d-81.3676755!2d19.2985383!3e2");
+    }
   }
 }
 </script>
